769d4602c4af21fd25f8fa679ca88e6bdda1e97b
ikatanyi <ikatanyi@gmail.com>
2023-09-06 16:23:14 +0300

    first commit

---
+++ src/main/java/com/pepeta/score/controller/ScoreController.java
@@ -0,0 +1,76 @@
+package com.pepeta.player.controller;
+
+import com.pepeta.player.dto.PlayerDto;
+import com.pepeta.player.model.Player;
+import com.pepeta.player.model.enumeration.Gender;
+import com.pepeta.player.service.PlayerService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+/**
+ *
+ * @author Kennedy Ikatanyi
+ */
+@RestController
+@Slf4j
+@RequestMapping("/api/customer")
+@RequiredArgsConstructor
+public class PlayerController {
+
+    private final PlayerService service;
+
+
+    @PostMapping
+    public ResponseEntity<?> createPlayer(@Valid @RequestBody PlayerDto resourceDto) {
+        PlayerDto dto = service.createPlayer(resourceDto).toPlayerDto();
+        return ResponseEntity.ok(dto);
+
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> fetchPlayerById(@PathVariable(value = "id") Long id) {
+        PlayerDto dto = service.fetchPlayerByIdOrThrow(id).toPlayerDto();
+        return ResponseEntity.ok(dto);
+    }
+
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updatePlayer(@PathVariable(value = "id") Long id, @Valid @RequestBody PlayerDto playerDto) {
+
+        Player resource = service.updatePlayer(id, playerDto);
+        return ResponseEntity.ok(resource.toPlayerDto());
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deletePlayer(@PathVariable(value = "id") Long id) {
+        service.deletePlayer(id);
+        return ResponseEntity.ok("Player deleted successfully");
+
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllPlayers(
+            @RequestParam(value = "gender", required = false) final Gender gender,
+            @RequestParam(value = "email", required = false) final String email,
+            @RequestParam(value = "name", required = false) final String name,
+            @RequestParam(value = "phoneNumber", required = false) final String phoneNumber,
+            @RequestParam(value = "page", defaultValue = "0",required = false) Integer page,
+            @RequestParam(value = "pageSize", defaultValue = "20",required = false) Integer size) {
+        page = page>=1 ? page-1 : page;
+        Pageable pageable = PageRequest.of(page, size);
+
+        Page<PlayerDto> pagedList = service.fetchPlayers(gender, email, name, phoneNumber, pageable).map(u -> u.toPlayerDto());
+        return ResponseEntity.ok(pagedList);
+    }
+
+}
---
+++ src/main/java/com/pepeta/score/data/ScoreUpdateDto.java
@@ -0,0 +1,30 @@
+package com.pepeta.score.data;
+
+import com.pepeta.score.model.Score;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class ScoreDto {
+    @Schema(hidden = true)
+    private Long id;
+    private Integer score;
+    @NotEmpty(message = "Order Id is required")
+    private Long playerId;
+    @Schema(hidden = true)
+    private String playerName;
+    @Schema(hidden = true)
+    private String product;
+    private Integer rank;
+
+    public Score toScore(){
+        Score score = new Score();
+        score.setScore(this.getScore());
+        return score;
+    }
+}
